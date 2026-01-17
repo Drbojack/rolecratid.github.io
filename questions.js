ade9b4e1e66058df06645e0815c91271f7c1f4dd
Drbojack <orlandokang.business@gmail.com>
2026-01-16 20:42:43 -0700

    questions.js

--- questions.js
+++ questions.js
@@ -31,13 +31,13 @@ export const config = {
       "Trainer": "https://www.rolecraftid.com/trainer"
     },
     sdt: {
-      "Freedom / Autonomy": "https://www.rolecraftid.com/sdt",
-      "Mastery / Competence": "https://www.rolecraftid.com/sdt",
-      "Meaning / Relatedness": "https://www.rolecraftid.com/sdt"
+      "Freedom / Autonomy": "https://www.rolecraftid.com/freedom",
+      "Mastery / Competence": "https://www.rolecraftid.com/mastery",
+      "Meaning / Relatedness": "https://www.rolecraftid.com/meaning"
     },
     overview: {
-      rcid: "https://www.rolecraftid.com/test-results",
-      sdt: "https://www.rolecraftid.com/sdt",
+      rcid: "https://www.rolecraftid.com/results-overview",
+      sdt: "https://www.rolecraftid.com/results-overview",
       contact: "https://www.rolecraftid.com/contact"
     }
   },
